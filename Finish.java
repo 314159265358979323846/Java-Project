80d1194a729e7a1b5311966ce447d212cf0029ec
314159265358979323846 <harry.061511@gmail.com>
2020-06-08 00:40:39 +0800

    Add files via upload

---
+++ Finish.java
@@ -0,0 +1,49 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Finish extends JFrame
+{
+	JLabel label;
+	JButton button=new JButton("Quit");
+	private int win;
+	public Finish(int win)
+	{
+		setResizable(false);
+		setSize(600,600);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setLayout(null);
+		getContentPane().setBackground(Color.BLACK);
+		setUndecorated(true);
+		setVisible(true);
+		ImageIcon finish=new ImageIcon("img/finish.png");
+		JLabel img=new JLabel(finish);
+		getContentPane().add(img,null);
+		img.setBounds(0,150,600,300);
+		this.win=win;
+		if(win==1)
+			label=new JLabel("Player 1 Win");
+		else if(win==2)
+			label=new JLabel("Player 2 Win");
+		else if(win==3)
+			label=new JLabel("Computer Win");
+		getContentPane().add(label,null);
+		label.setBounds(150,50,400,100);
+		label.setFont(new Font("Time News Roman",Font.BOLD,40));
+		label.setForeground(Color.RED);
+		getContentPane().add(button,null);
+		button.setBounds(150,450,300,100);
+		button.setFont(new Font("Time News Roman",Font.BOLD,32));
+		button.setBackground(Color.BLACK);
+		button.setForeground(Color.WHITE);
+		button.addActionListener(new ActionListener()
+			{
+				@Override
+				public void actionPerformed(ActionEvent e)
+				{
+					System.exit(0);
+				}
+			});
+	}
+}
